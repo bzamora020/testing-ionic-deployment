12a94ddaf6346334e4a6bfa4ee88ea11fd6a6a18
werner <elmer@werners-MacBook-Air.local>
2021-07-06 00:38:50 -0700

    saving final ionic challenge

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  name: "elmer";
+  infoObj: any = {
+    name: "Elmer Vasquez", 
+    school: "UC SANTA CRUZ, Junior", 
+    major: "Computer Science", 
+    bio: "I enjoy programming, anime, drawing, hanging with friends and family, and I love listening to chill music."
+  }
 
   constructor() {}
 
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+import friendsObj from './friendsObj';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
-  constructor() {}
+  friendsList: any[];
+  // friendsList : any[] = friendsObj;
+  constructor() {
+    this.friendsList = friendsObj;
+    console.log(friendsObj);
+    console.log(this.friendsList[0].name);
+  }
 
 }
